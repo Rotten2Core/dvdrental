0089ad3d8f56b413c07d16e837aedd8e75e29e6a
Rotten2Core <kozyr.sergej@gmail.com>
2020-11-13 16:58:39 +0200

    Actor's function was modified

--- application.py
+++ application.py
@@ -1,8 +1,19 @@
-import datetime
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_CONN')
+db = SQLAlchemy(app)
+
+
+class Actor(db.Model):
+    actor_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(45), nullable=False)
+    last_name = db.Column(db.String(45), nullable=False)
+    last_update = db.Column(db.DateTime(timezone=False), nullable=False)
 
 
 @app.context_processor
@@ -13,21 +24,33 @@ def utility_processor():
 
         return value.strftime('%d.%m.%Y %H:%M:%S')
 
-    return dict(format_value=format_value)
+    def format_get_params(**kwargs) -> str:
+        params = []
+        request_args = dict(request.args)
+        request_args.update(kwargs)
+        for key, value in request_args.items():
+            params.append('{}={}'.format(key, value))
+
+        return '?{}'.format('&'.join(params))
+
+    return dict(format_value=format_value, format_get_params=format_get_params)
 
 
-@app.route('/actor/')
+@app.route('/actors/')
 def actor():
-    actors = [{
-        'id': 17,
-        'first_name': 'Serhii',
-        'last_name': 'Kozyr',
-        'last_update': datetime.datetime.now(),
-    }, {
-        'id': 13,
-        'first_name': 'Mykyta',
-        'last_name': 'Velykanov',
-        'last_update': datetime.datetime.now() - datetime.timedelta(minutes=30),
-    }]
+    sort_keys = {
+        'id': Actor.actor_id,
+        'first_name': Actor.first_name,
+        'last_name': Actor.last_name,
+        'last_update': Actor.last_update,
+    }
+    sort = request.args.get('sort', 'id')
+    page = int(request.args.get('page', 1))
+    if page <= 0:
+        page = 1
 
-    return render_template('list.html', title='Actors', data=actors)
+    page_size = int(request.args.get('page_size', 25))
+    offset = page_size * (page - 1)
+    actors = Actor.query.order_by(sort_keys.get(sort, sort_keys['id'])).limit(25).offset(offset).all()
+
+    return render_template('list.html', title='Actors', data=actors, page=page, sort=sort)
