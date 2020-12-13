c58eecccdb69ff868054885679256a0b13be5c84
velykanov <nikitavelykanov@gmail.com>
2020-12-13 22:03:34 +0200

    XLSX streaming

--- modules/actors/__init__.py
+++ modules/actors/__init__.py
@@ -1,11 +1,12 @@
 import datetime
-import io
-
-import xlsxwriter
+import zipfile
 
 from flask import Blueprint, redirect, render_template, request, Response, stream_with_context
-
 from flask_sqlalchemy import BaseQuery
+from xlsx_streaming.render import _get_column_letter
+from xlsx_streaming.streaming import zip_to_zipstream
+
+from xlsx_streaming.xlsx_template import DEFAULT_TEMPLATE
 
 from common import paginate_list, render_item
 from db import get_db
@@ -101,22 +102,41 @@ def actors_export(file_extension):
         yield from map(Actor.to_csv_row, actors_gen)
 
     def generate_xlsx():
-        output = io.BytesIO()
-        workbook = xlsxwriter.Workbook(output, {'in_memory': True})
-        sheet = workbook.add_worksheet('Actors')
-        row = 0
-        for actor in Actor.query.yield_per(100):
-            sheet.write_row(row, 0, actor.to_xlsx_row())
-            row += 1
+        def render_worksheet(data):
+            yield (
+                b'<worksheet'
+                b' xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"'
+                b' xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">'
+                b'<sheetData>'
+            )
 
-        workbook.close()
-        headers['Content-Length'] = str(output.tell())
-        output.seek(0)
+            yield from (
+                render_row(row.to_xlsx_row(), row_number).encode('utf-8')
+                for row_number, row in enumerate(data, start=1)
+            )
 
-        data = output.read(8192)
-        while data:
-            yield data
-            data = output.read(8192)
+            yield b'</sheetData></worksheet>'
+
+        def render_row(values, line_number):
+            cells = (
+                f'<c r="{_get_column_letter(position)}{line_number}" t="inlineStr"><is><t>{value}</t></is></c>'
+                for position, value in enumerate(values, start=1)
+            )
+
+            return f"""<row r="{line_number}">{''.join(cells)}</row>"""
+
+        zip_template = zipfile.ZipFile(DEFAULT_TEMPLATE, mode='r')
+        sheet_name = 'xl/worksheets/sheet1.xml'
+        zipped_stream = zip_to_zipstream(zip_template, exclude=[sheet_name])
+
+        worksheet_stream = render_worksheet(Actor.query.yield_per(100))
+        zipped_stream.write_iter(
+            arcname=sheet_name,
+            iterable=worksheet_stream,
+            compress_type=zipfile.ZIP_DEFLATED,
+        )
+
+        return zipped_stream
 
     return Response(
         stream_with_context(generate_xlsx() if file_extension == 'xlsx' else generate_csv()),
